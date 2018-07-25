1e831bcdd57f4a8b662bb9b5e7d7c2e62065a34b
Fernando Barrueto <37130555+Lirhhack@users.noreply.github.com>
2018-07-25 15:49:26 -0500

    Update La paradoja del cumpleaños.py

--- La paradoja del cumpleaños.py
+++ La paradoja del cumpleaños.py
@@ -1,22 +1,15 @@
 import random  # Importamos la libreria random
 
-
-def generar_cumpleaños():  # creamos una funcion que genera un cumpleaños
-    mes = random.randint(1, 12)  # Agarra un mes cualquiera
-    dia = random.randint(1, 31)  # Agarra un dia cualquiera
-    return dia, mes  # retorna el dia y el mes
-
+def generar_cumpleaños(): 
+    mes = random.randint(1, 12)  
+    dia = random.randint(1, 31)  
+    return dia, mes 
 
 n = int(input('Ingrese la cantidad de personas que desea probar : '))
-# la linea de arriba indica la cantidad de cumpleaños que crearemos o
-# mejor dicho, la cantidad de personas que habran en la sala
-List = []  # creamos una lista vacia
-for x in range(0, n):  # hacemos un for desde 0 hasta la cantidad ingresada
-    List.append(generar_cumpleaños())  # Agregamos cada cumpleaños generado
-    #  a la lista
-print(List)  # Imprimos la lista para ver que paso
-
-
+List = []  
+for x in range(0, n):  
+    List.append(generar_cumpleaños())  
+    
 if len(List) != len(list((set(List)))):
     print('Hay dos cumpleaños , felicidades')
 else:
